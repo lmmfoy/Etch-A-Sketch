0846a15043831886d923cb7554bf911fcab31db7
lea <46614864+lmmfoy@users.noreply.github.com>
2022-02-11 15:14:52 -0500

    Updated the buttonand rearranged code into functions  to set up new grid based on input

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const container = document.getElementById("container");
 
+let boxSize = 16;
 
-for (let i = 0; i < 256; i++) {
-        let div = document.createElement("div");
-        div.setAttribute("class", "boxes");
-        
-        container.appendChild(div);  
+function makeGrid() {
+    container.style.setProperty("--grid-number", boxSize);
+    for (let i = 0; i < (boxSize * boxSize); i++) {
+            let div = document.createElement("div");
+            div.setAttribute("class", "boxes");
+            
+            container.appendChild(div);  
+    }
 }
 
-const boxes = document.querySelectorAll(".boxes");
+makeGrid()
 
-boxes.forEach(box => {
-     box.addEventListener("mouseover", function(e) {
-         e.target.classList.add("color");
-     }
-     )
- })
+
+function colourfulBoxes() {
+    const boxes = document.querySelectorAll(".boxes");
+    boxes.forEach(box => {
+        box.addEventListener("mouseover", function(e) {
+            e.target.classList.add("color");
+        }
+        )
+    })
+}
+
+colourfulBoxes()
 
 const button = document.getElementById("restart") 
 
 button.addEventListener("click", function() {
+    const boxes = document.querySelectorAll(".boxes");
     boxes.forEach(box => {
         box.classList.remove("color");
     }) 
+    boxSize = prompt("Please choose a number from 2 - 100.", 16);
+    console.log(boxSize);
+    while (isNaN(boxSize) | boxSize < 2 | boxSize > 100) {
+        boxSize = prompt("Please choose a number from 2 - 100.", 16);
+    }
+    container.textContent = "";
+    makeGrid();
+    colourfulBoxes();
+
 })
 
