7b5a246c138c80725129d7938ba43417db99eab1
lea <46614864+lmmfoy@users.noreply.github.com>
2022-02-11 12:24:13 -0500

    Made divs change colour on mouseover

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ for (let i = 0; i < 256; i++) {
         div.setAttribute("class", "boxes");
         
         container.appendChild(div);  
-    
 }
+
+const boxes = document.querySelectorAll(".boxes");
+
+boxes.forEach(box => {
+     box.addEventListener("mouseover", function(e) {
+         console.log("hi");
+         e.target.style.backgroundColor = "purple";
+     }
+     )
+ })
 
