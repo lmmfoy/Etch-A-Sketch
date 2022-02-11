b057a4016a9868db5d69e0a1cb8614dda057e92c
lea <46614864+lmmfoy@users.noreply.github.com>
2022-02-11 18:45:14 -0500

    Added function to change hex values to rgb and make boxes darker when hovered over multiple times

--- script.js
+++ script.js
@@ -15,16 +15,49 @@ function changeColour() {
     const boxes = document.querySelectorAll(".boxes");
     boxes.forEach(box => {
         box.addEventListener("mouseover", function(e) {
-            randomColour(box);            
-            e.target.classList.add("color");
+            randomColour(box);  
+            e.target.classList.add("colour");
         })
     })
 }
 
 function randomColour(box) {
     let colour = "#" + Math.floor(Math.random()*16777215).toString(16);
-    box.style.setProperty("--box-colour", colour);
+    console.log(colour);
+    if (getComputedStyle(box).getPropertyValue("--box-colour") == "black") {
+        box.style.setProperty("--box-colour", colour);
+    } else {
+        box.style.setProperty("--box-colour", toRGB(box));
+        console.log(toRGB(box));
+    }
 }
+
+function toRGB(box) {
+    let r = 0, g = 0, b = 0;
+    let boxColour = getComputedStyle(box).getPropertyValue("--box-colour");
+
+    if (boxColour.length == 4) {
+        r = "0x" + boxColour[1] + boxColour[1];
+        g = "0x" + boxColour[2] + boxColour[2];
+        b = "0x" + boxColour[3] + boxColour[3];
+    } else if (boxColour.length == 7) {
+        r = "0x" + boxColour[1] + boxColour[2];
+        g = "0x" + boxColour[3] + boxColour[4];
+        b = "0x" + boxColour[5] + boxColour[6];
+    }
+
+    return "rgb("+ (+r - 25.5) + "," + (+g - 25.5) + "," + (+b - 25.5) + ")";
+}
+
+
+
+
+// function blackFade(colour) {
+//     let r = parseInt(colour.substring(1, 4), 16);
+//     r *= .9    
+//}
+
+
 
 makeGrid()
 changeColour()
